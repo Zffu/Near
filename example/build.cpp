a5e337bcf39abff50b65db19d19850f100777ddb
Zffu <103074097+Zffu@users.noreply.github.com>
2025-10-11 23:10:18 +0200

    feat: added example

---
+++ example/build.cpp
@@ -0,0 +1,25 @@
+#include <near.hpp>
+#include <compiler.hpp>
+#include <files.hpp>
+#include <gcc.hpp>
+
+using namespace near;
+using namespace near::compilers;
+
+void build_normal() {
+	GCCCompiler compiler("c++20");
+	
+	compiler.optimized_settings();
+
+	FolderFileSource source = FolderFileSource("src");
+	source.add_ext(".cpp");
+	source.add_ext(".c");
+
+	CompilerOutput output("out", EXECUTABLE);
+	compiler.build(&source, &output);
+}
+
+int main(int argc, char** argv) {
+	Near::register_profile("build", build_normal);
+	Near::init(argc, argv);
+}
