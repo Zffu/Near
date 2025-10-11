c2f6721f7bd93b14fd023e341253727bc2f5cc57
Zffu <103074097+Zffu@users.noreply.github.com>
2025-10-11 21:19:13 +0200

    feat: added lexer

--- includes/near/var.hpp
+++ includes/near/var.hpp
@@ -1,7 +1,30 @@
 namespace near {
 
-class NearValueLikely {
+enum NearType {
+	INTEGER,
+	BOOLEAN,
+	STRING,
+
+	FILE_STREAM,
+	COMPILER
+};
+
+class NearValue {
+protected:
+	void* data;
+
 public:
+	NearType type;
+	bool is_array;
+	
+	NearValue(NearType type, bool is_array, void* data) {
+		this->type = type;
+		this->is_array = is_array;
+		this->data = data;
+	}
+};
+
+class NearInt: public NearValue {
 
 }
 
