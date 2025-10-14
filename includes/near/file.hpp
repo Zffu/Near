616435e9e3b377a8db0068cf50bea93af62c136f
Zffu <103074097+Zffu@users.noreply.github.com>
2025-10-14 22:05:19 +0200

    feat: added decl

--- includes/near/file.hpp
+++ includes/near/file.hpp
@@ -14,12 +14,20 @@ namespace near::io {
 class FileStream {
 protected:
 	std::vector<fs::path> paths;
+	std::vector<std::string> extensions;
+
+	void collect_ext_in_path(fs::path path, std::string ext);
 
 public:
 	std::vector<fs::path> directories;
 
-	FileStream();
+	FileStream() {};
 	FileStream(fs::path dir);
+
+	void add_directory(fs::path dir);
+	void add_file(fs::path file);
+
+	void add_file_extension(std::string extension);
 
 	VecIterator<fs::path> begin() {
 		return VecIterator<fs::path>(this->paths.begin());
---
+++ src/file.cpp
@@ -0,0 +1,27 @@
+#include <near/file.hpp>
+
+using namespace near::io;
+
+FileStream::FileStream(fs::path dir) {
+	this->directories.push_back(dir);
+}
+
+void FileStream::add_directory(fs::path dir) {
+	this->directories.push_back(dir);
+
+	for(std::string ext : this->extensions) {
+		this->collect_ext_in_path(dir, ext);
+	}
+}
+
+void FileStream::add_file(fs::path file) {
+	this->paths.push_back(file);
+}
+
+void FileStream::add_file_extension(std::string extension) {
+	this->extensions.push_back(extension);
+
+	for(fs::path path : this->directories) {
+		this->collect_ext_in_path(path, extension);
+	}
+}
